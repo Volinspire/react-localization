f369506172da94af7c62b93afd4d734994a7985d
Brent Luehr <brent@nerdcon.com>
2020-06-04 11:39:48 -0700

    Better SSR compat w/navigator checking

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,29 @@
 export function getInterfaceLanguage() {
-  if (!!navigator && !!navigator.language) {
+  if (typeof navigator !== "undefined" && !!navigator.language) {
     return navigator.language;
-  } else if (!!navigator && !!navigator.languages && !!navigator.languages[0]) {
+  } else if (
+    typeof navigator !== "undefined" &&
+    !!navigator.languages &&
+    !!navigator.languages[0]
+  ) {
     return navigator.languages[0];
-  } else if (!!navigator && !!navigator.userLanguage) {
+  } else if (typeof navigator !== "undefined" && !!navigator.userLanguage) {
     return navigator.userLanguage;
-  } else if (!!navigator && !!navigator.browserLanguage) {
+  } else if (typeof navigator !== "undefined" && !!navigator.browserLanguage) {
     return navigator.browserLanguage;
   }
-  return 'en-US';
+  return "en-US";
 }
 
 export function validateTranslationKeys(translationKeys) {
   const reservedNames = [
-    '_interfaceLanguage',
-    '_language',
-    '_defaultLanguage',
-    '_defaultLanguageFirstLevelKeys',
-    '_props',
+    "_interfaceLanguage",
+    "_language",
+    "_defaultLanguage",
+    "_defaultLanguageFirstLevelKeys",
+    "_props",
   ];
-  translationKeys.forEach(key => {
+  translationKeys.forEach((key) => {
     if (reservedNames.indexOf(key) !== -1) {
       throw new Error(`${key} cannot be used as a key. It is a reserved word.`);
     }
